75b844ea3fd8f1aa80391a2fa00a5a48f6e6c9e8
xlgzsgf <2636639786@qq.com>
2024-08-14 23:35:32 +0800

    fix: :bug: fix `z` not defined

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,24 @@
-import {defineCollection} from 'astro:content';
-import {docsSchema, i18nSchema} from '@astrojs/starlight/schema';
+import { defineCollection, z } from "astro:content";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 
 const faq = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		keywords: z.string().or(z.array(z.string().or(z.number())).optional()),
-		// Transform string to Date object
-		date: z.string(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		author: z.string().optional(),
-		category: z.string().optional()
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    keywords: z.string().or(z.array(z.string().or(z.number())).optional()),
+    // Transform string to Date object
+    date: z.string(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    author: z.string().optional(),
+    category: z.string().optional(),
+  }),
 });
 
 const docs = defineCollection({ schema: docsSchema() });
 
-
 export const collections = {
-	docs,
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-	faq,
+  docs,
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
+  faq,
 };
